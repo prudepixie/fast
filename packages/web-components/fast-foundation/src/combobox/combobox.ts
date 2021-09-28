e8c895e5a4c896c3f3296024720c18a95214c2af
Kham Udom <khamu@microsoft.com>
2021-09-28 15:28:10 -0700

    update change log, add function and test to combobox

--- packages/web-components/fast-foundation/src/combobox/combobox.ts
+++ packages/web-components/fast-foundation/src/combobox/combobox.ts
@@ -271,6 +271,8 @@ export class Combobox extends FormAssociatedCombobox {
         if (this.value) {
             this.initialValue = this.value;
         }
+
+        this.handleUnsupportedDelegatesFocus();
     }
 
     /**
@@ -610,6 +612,24 @@ export class Combobox extends FormAssociatedCombobox {
             this.$emit("change");
         }
     }
+
+    /**
+     * Overrides the focus call for where delegatesFocus is unsupported.
+     * This check works for Chrome, Edge Chromium, FireFox, and Safari
+     * Relevant PR on the Firefox browser: https://phabricator.services.mozilla.com/D123858
+     */
+    private handleUnsupportedDelegatesFocus = () => {
+        // Check to see if delegatesFocus is supported
+        if (
+            window.ShadowRoot &&
+            !window.ShadowRoot.prototype.hasOwnProperty("delegatesFocus") &&
+            this.$fastController.definition.shadowOptions?.delegatesFocus
+        ) {
+            this.focus = () => {
+                this.control.focus();
+            };
+        }
+    };
 }
 
 /**
